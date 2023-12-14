5bc275e01664681e9d8918ee3103385f5e00d5e4
Antonio Boksic <antonio.boksic@contaq.it>
2023-12-14 13:58:08 +0100

    push prima di passare ai test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
-<script setup>
+<script>
 import HelloWorld from './components/HelloWorld.vue';
+import MainNav from './components/MainNav.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld: HelloWorld,
+    MainNav: MainNav
+  }
+};
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <MainNav />
+
+      <!-- <h1 class="text-lg">Hello, im in app vue</h1> -->
+      <!-- <HelloWorld msg="You did it!" /> -->
     </div>
   </header>
 </template>
+
+<style scoped>
+h1 {
+  color: purple;
+}
+</style>
